f0cf53c3817cedb03025e08ed22f08233c39664a
ACastello <a.castello.pas@gmail.com>
2016-11-19 17:32:20 +0100

    .

--- src/H4x.hs
+++ src/H4x.hs
@@ -1,5 +1,7 @@
 module H4x where
 
+import BoX11
+
 import Data.Word
 
 import Foreign
@@ -8,11 +10,13 @@ type HANDLE = Word64
 
 type PROCESS = Word32
 
-foreign import ccall "OpenProcess" 
-    openProcess :: Word32 -> Bool -> Word32 -> IO HANDLE
+-- foreign import ccall "OpenProcess" 
+    -- openProcess :: Word32 -> Bool -> Word32 -> IO HANDLE
 
-foreign import ccall "CloseHandle"
-    closeHandle :: HANDLE -> IO Bool
+-- foreign import ccall "CloseHandle"
+    -- closeHandle :: HANDLE -> IO Bool
+
+foreign import ccall "MessageBoxA" asd :: IO ()
 
 foreign import ccall "K32EnumProcesses"
     c_EnumProcesses :: Ptr PROCESS -> Word32 -> Ptr Word32 -> IO Bool
