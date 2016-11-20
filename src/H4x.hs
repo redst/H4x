6135eec677abaec1a6cf089f5615f1ef364ab2d1
ACastello <a.castello.pas@gmail.com>
2016-11-20 18:10:29 +0100

    added write functions

--- src/H4x.hs
+++ src/H4x.hs
@@ -1,5 +1,7 @@
 module H4x where
 
+import Control.Monad
+
 import Data.Word
 
 import Foreign
@@ -11,10 +13,10 @@ type PTR = LPTR
 type Pid = Word32
 
 
-memread :: Storable a => Pid -> PTR -> IO a
+memread :: (Integral ptr, Storable a) => Pid -> ptr -> IO a
 memread pid addr = do
     retPtr <- malloc :: Storable a => IO (Ptr a)
-    c_memread pid addr (castPtr retPtr) (sizeOfPtr retPtr)
+    c_memread pid (fromIntegral addr) (castPtr retPtr) (sizeOfPtr retPtr)
     ret <- peek retPtr
     free retPtr
     return ret
@@ -27,26 +29,43 @@ chainRead32 pid [lst] = memread pid (fromIntegral lst)
 chainRead32 pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
     \ptr'' -> chainRead32 pid ((ptr''+ptr'):ps)
 
-chainRead64 :: Storable a => Pid -> [LPTR] -> IO a
-chainRead64 pid [lst] = memread pid (fromIntegral lst) 
-chainRead64 pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
-    \ptr'' -> chainRead64 pid ((ptr''+ptr'):ps)
+chainRead :: Storable a => Pid -> [LPTR] -> IO a
+chainRead pid [lst] = memread pid (fromIntegral lst) 
+chainRead pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
+    \ptr'' -> chainRead pid ((ptr''+ptr'):ps)
+
+readoff :: Storable a => Pid -> PTR -> PTR -> IO a
+readoff pid addr offset = memread pid (addr + offset)
+
+readoff32 :: Storable a => Pid -> SPTR -> SPTR -> IO a
+readoff32 pid addr offset = memread pid $ fromIntegral (addr + offset)
+
+chainOffset :: (Storable ptr, Integral ptr) => Pid -> [ptr] -> IO PTR
+chainOffset _ [] = return 0
+-- chainOffset _ [addr] = return (fromIntegral addr)
+chainOffset pid (add:adds) = fromIntegral <$> foldl step (return add) adds
+    where
+        step :: (Storable a, Integral a) => IO a -> a -> IO a
+        step baseM offset = do
+            base <- baseM
+            target <- memread pid base
+            return (target+offset)
 
 
-memwrite :: Storable a => Pid -> PTR -> a -> IO ()
+
+memwrite :: (Integral ptr, Storable a) => Pid -> ptr -> a -> IO ()
 memwrite pid addr elem = with elem $ \ptr -> do
-    c_memwrite pid addr (castPtr ptr) (sizeOf elem)    
+    c_memwrite pid (fromIntegral addr) (castPtr ptr) (sizeOf elem)    
 
 foreign import ccall "memwrite"
     c_memwrite :: Pid -> PTR -> Ptr () -> Int -> IO ()
 
-chainWrite32 :: Storable a => Pid -> [SPTR] -> a -> IO ()
-chainWrite32 pid lst val = chainRead32 pid (init lst) >>=
-    \addr -> memwrite pid addr val
 
-chainWrite64 :: Storable a => Pid -> [LPTR] -> a -> IO ()
-chainWrite64 pid lst val = chainRead64 pid (init lst) >>=
-    \addr -> memwrite pid addr val
+writeOff :: Storable a => Pid -> PTR -> PTR -> IO a
+writeOff pid addr offset = memread pid (addr + offset)
+
+writeoff32 :: Storable a => Pid -> SPTR -> SPTR -> IO a
+writeoff32 pid addr offset = memread pid $ fromIntegral (addr + offset)
 
 
 
