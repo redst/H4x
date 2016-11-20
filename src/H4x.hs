155237044eb4a91106b01a5a46ee2e937838452a
ACastello <a.castello.pas@gmail.com>
2016-11-20 03:47:27 +0100

    .

--- src/H4x.hs
+++ src/H4x.hs
@@ -27,13 +27,28 @@ chainRead32 pid [lst] = memread pid (fromIntegral lst)
 chainRead32 pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
     \ptr'' -> chainRead32 pid ((ptr''+ptr'):ps)
 
+chainRead64 :: Storable a => Pid -> [LPTR] -> IO a
+chainRead64 pid [lst] = memread pid (fromIntegral lst) 
+chainRead64 pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
+    \ptr'' -> chainRead64 pid ((ptr''+ptr'):ps)
+
 
 memwrite :: Storable a => Pid -> PTR -> a -> IO ()
 memwrite pid addr elem = with elem $ \ptr -> do
-    
+    c_memwrite pid addr (castPtr ptr) (sizeOf elem)    
 
 foreign import ccall "memwrite"
     c_memwrite :: Pid -> PTR -> Ptr () -> Int -> IO ()
+
+chainWrite32 :: Storable a => Pid -> [SPTR] -> a -> IO ()
+chainWrite32 pid lst val = chainRead32 pid (init lst) >>=
+    \addr -> memwrite pid addr val
+
+chainWrite64 :: Storable a => Pid -> [LPTR] -> a -> IO ()
+chainWrite64 pid lst val = chainRead64 pid (init lst) >>=
+    \addr -> memwrite pid addr val
+
+
 
 sizeOfPtr :: Storable a => Ptr a -> Int
 sizeOfPtr = sizeOf . (undefined :: Ptr a -> a)
