14d9feaab15ec1cbfe71cf504df22d738097e2b6
ACastello <a.castello.pas@gmail.com>
2016-11-21 02:00:59 +0100

    .

--- src/H4x.hs
+++ src/H4x.hs
@@ -2,18 +2,50 @@ module H4x where
 
 import Control.Monad
 
+import Data.Foldable
+import Data.Serialize
 import Data.Word
 
 import Foreign
 import Foreign.C
 
+import System.Posix.Types
+
 type LPTR = Word64
 type SPTR = Word32
 type PTR = LPTR
-type Pid = Word32
+
+data Point = Point
+    { p_x   :: Maybe Float
+    , p_y   :: Maybe Float
+    , p_z   :: Maybe Float
+    , p_th  :: Maybe Float }
+
+instance Show Point where
+    show (Point x y z th) = 
+        "(" ++ unmaybe x ++ ", " ++ unmaybe y ++ ", " ++ unmaybe z ++ ") " 
+        ++ unmaybe th ++ " rad"
+        where
+            unmaybe = maybe "_" show 
+
+instance Storable Point where
+    sizeOf _ = 20
+    alignment _ = 8
+    peek ptr = do
+        [x,y,z,r] <- fmap Just <$> traverse (peekByteOff ptr) [0,4,8,16]
+        return $ Point x y z r
+    poke ptr (Point x y z r) = do
+        traverse_ (pokeByteOff ptr 0) x
+        traverse_ (pokeByteOff ptr 4) y
+        traverse_ (pokeByteOff ptr 8) z
+        traverse_ (pokeByteOff ptr 16) r
+
+instance Serialize Point where
+    get = liftM4 Point get get get get
+    put (Point x y z r) = put x >> put y >> put z >> put r
 
 
-memread :: (Integral ptr, Storable a) => Pid -> ptr -> IO a
+memread :: (Integral ptr, Storable a) => CPid -> ptr -> IO a
 memread pid addr = do
     retPtr <- malloc :: Storable a => IO (Ptr a)
     c_memread pid (fromIntegral addr) (castPtr retPtr) (sizeOfPtr retPtr)
@@ -22,27 +54,13 @@ memread pid addr = do
     return ret
 
 foreign import ccall "memread"
-    c_memread :: Pid -> PTR -> Ptr () -> Int -> IO ()
+    c_memread :: CPid -> PTR -> Ptr () -> Int -> IO ()
 
-chainRead32 :: Storable a => Pid -> [SPTR] -> IO a
-chainRead32 pid [lst] = memread pid (fromIntegral lst) 
-chainRead32 pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
-    \ptr'' -> chainRead32 pid ((ptr''+ptr'):ps)
+chainRead :: (Integral ptr, Storable ptr, Storable a) => CPid -> [ptr] -> IO a
+chainRead pid adds = chainOffset pid adds >>= memread pid
 
-chainRead :: Storable a => Pid -> [LPTR] -> IO a
-chainRead pid [lst] = memread pid (fromIntegral lst) 
-chainRead pid (ptr:ptr':ps) = memread pid (fromIntegral ptr) >>=
-    \ptr'' -> chainRead pid ((ptr''+ptr'):ps)
-
-readoff :: Storable a => Pid -> PTR -> PTR -> IO a
-readoff pid addr offset = memread pid (addr + offset)
-
-readoff32 :: Storable a => Pid -> SPTR -> SPTR -> IO a
-readoff32 pid addr offset = memread pid $ fromIntegral (addr + offset)
-
-chainOffset :: (Storable ptr, Integral ptr) => Pid -> [ptr] -> IO PTR
+chainOffset :: (Storable ptr, Integral ptr) => CPid -> [ptr] -> IO ptr
 chainOffset _ [] = return 0
--- chainOffset _ [addr] = return (fromIntegral addr)
 chainOffset pid (add:adds) = fromIntegral <$> foldl step (return add) adds
     where
         step :: (Storable a, Integral a) => IO a -> a -> IO a
@@ -51,21 +69,12 @@ chainOffset pid (add:adds) = fromIntegral <$> foldl step (return add) adds
             target <- memread pid base
             return (target+offset)
 
-
-
-memwrite :: (Integral ptr, Storable a) => Pid -> ptr -> a -> IO ()
+memwrite :: (Integral ptr, Storable a) => CPid -> ptr -> a -> IO ()
 memwrite pid addr elem = with elem $ \ptr -> do
     c_memwrite pid (fromIntegral addr) (castPtr ptr) (sizeOf elem)    
 
 foreign import ccall "memwrite"
-    c_memwrite :: Pid -> PTR -> Ptr () -> Int -> IO ()
-
-
-writeOff :: Storable a => Pid -> PTR -> PTR -> IO a
-writeOff pid addr offset = memread pid (addr + offset)
-
-writeoff32 :: Storable a => Pid -> SPTR -> SPTR -> IO a
-writeoff32 pid addr offset = memread pid $ fromIntegral (addr + offset)
+    c_memwrite :: CPid -> PTR -> Ptr () -> Int -> IO ()
 
 
 
